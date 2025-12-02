1aeea9ea8be977ffa991876315fcb45d3fc9b2c4
Arboy Magomba <arboydev@gmail.com>
2025-12-02 13:00:51 -0600

    Initialized Vercel Analytics by installing the analytics package and adding it to the main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "LumiPact - AI Contract Studio | by LumiarLabs",
@@ -31,6 +32,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
